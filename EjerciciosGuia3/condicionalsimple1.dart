c92a5003295ce430c8e44f644b2f5a20d37aca12
Juan1511 <j15020611zulu@gmail.com>
2024-04-23 11:52:40 -0500

    se puso mas contenido

--- EjerciciosGuia3/condicionalsimple1.dart
+++ EjerciciosGuia3/condicionalsimple1.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 void main(List<String> args){
   // JUAN MANUEL ZULUAGA RINCON - CONDICIONAL SIMPLE 01
   /*
-
+  El jefe del personal de operación de la industria aceitera Móvil desea calcular el sueldo neto de sus empleados bajo las siguientes normas, solicitar el nombre del empleado, número de horas trabajadas y la cuota por hora trabajada, para calcular el sueldo neto del empleado, se le otorga un incentivo del  5%  si  el  empleado  trabajó  más  de  40  horas.  Imprimir  el  nombre  del  empleado  y  su  sueldo. 
   */
 
   //DEFINICION DE VARIABLES
