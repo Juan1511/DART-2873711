cd4d3bf5d97ddb9f3d7b51e5b6900193670b5600
Juan1511 <j15020611zulu@gmail.com>
2024-04-25 18:30:29 -0500

    hola

---
+++ EjerciciosGuia3/Condicionalesimples/condicionalesSimples7.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  //JUAN MANUEL ZULUAGA - CONDICIONALES SIMPLES 07
+  /*
+  Una compañía de seguros esta abriendo un depto. de finanzas y estableció un programa para captar clientes, que consiste en lo siguiente: Si el monto por el que se efectúa la fianza es menor que $50 000 la cuota a pagar será por el 3% del monto, y si el monto es mayor que $50 000 la cuota a pagar será el 2% del monto. La compañia desea determinar cual será la cuota que debe pagar un cliente.
+  */
+
+  //DECLARACION DE VARIABLES
+  double fianza, cuota;
+
+  //ENTRADA DE DATOS
+  print("ingrese el monto de la fianza");
+  fianza = double.parse(stdin.readLineSync()!);
+
+  //PROCESO
+cuota = fianza*0.03;
+if(fianza >= 50000){
+  cuota = fianza*0.02;
+}
+
+//SALIDA
+print("la cuota a pagar es de $cuota");
+}
