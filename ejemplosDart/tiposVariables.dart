730090c2b8874820a7ad9a593090085f6c3cfdc4
Juan1511 <j15020611zulu@gmail.com>
2024-04-30 08:30:29 -0500

    se arreglo

--- ejemplosDart/tiposVariables.dart
+++ ejemplosDart/tiposVariables.dart
@@ -10,11 +10,9 @@ void main() {
   String nombre;
   int edad;
   double altura; //es el float
-  bool estado;
   nombre = "Juan";
   edad = 18;
   altura = 1.70;
-  estado = true;
   print("Su nombre es $nombre");
   print("Su altura es $altura y su edad es $edad");
 }
