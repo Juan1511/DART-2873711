747626c8c2cedc854293d3a3aa595d5a3022bb95
Juan1511 <j15020611zulu@gmail.com>
2024-05-02 10:40:26 -0500

    se completo

---
+++ EjerciciosGuia3/condicionalesMultiples/condicionalesMultiples4.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  //JUAN MANUEL ZULUAGA - CONDICIONALES MULTIPLES 04
+
+  //DEFINICION DE VARIABLES 
+
+  int numero;
+  double mod, x, resultado;
+
+  //ENTRADA DE DATOS
+  print("ingrese el valor de X:");
+  x = double.parse(stdin.readLineSync()!);
+
+  //PROCESO
+ 
+    
+  }
